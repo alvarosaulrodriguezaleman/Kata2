9d1e4075523b29624bad89fc944b1200e2a0d3cd
Álvaro Saúl Rodríguez Alemán <>
2018-09-27 09:41:20 +0100

    Kata2 v5.0 - Cambio de tipo de datos de array de int a String.

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -5,12 +5,12 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        Integer[] array = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6};
+        String[] array = {"Álvaro", "Álvaro", "Álvaro", "Nahuel", "Nahuel", "Ana", "Ana", "Juan", "Ana", "Ana"};
         
         Histogram histo = new Histogram(array);
-        Map<Integer, Integer> histogr = histo.getHistogram();
+        Map<String, Integer> histogr = histo.getHistogram();
         
-        for (Map.Entry<Integer,Integer> entry : histogr.entrySet()) {
+        for (Map.Entry<String,Integer> entry : histogr.entrySet()) {
             System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
     }
