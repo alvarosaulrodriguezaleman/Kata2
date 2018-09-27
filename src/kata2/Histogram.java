45d046688121ca156be6eb33e93e9c1675c9ee2e
Álvaro Saúl Rodríguez Alemán <>
2018-09-27 09:34:13 +0100

    Kata2 v4.0 - Uso de tipos genéricos para los datos almacenados en la clase Histogram.

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -3,20 +3,20 @@ package kata2;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Histogram {
-    private final int[] array;
+public class Histogram<T> {
+    private final T[] array;
 
-    public Histogram(int[] array) {
+    public Histogram(T[] array) {
         this.array = array;
     }
 
-    public int[] getArray() {
+    public T[] getArray() {
         return array;
     }
     
-    public Map<Integer, Integer> getHistogram() {
-        Map<Integer, Integer> histogram = new HashMap<>();
-        for (int i : array) {
+    public Map<T, Integer> getHistogram() {
+        Map<T, Integer> histogram = new HashMap<>();
+        for (T i : array) {
             histogram.put(i, histogram.containsKey(i) ? histogram.get(i) + 1 : 1);
         }
         return histogram;
--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6};
+        Integer[] array = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6};
         
         Histogram histo = new Histogram(array);
         Map<Integer, Integer> histogr = histo.getHistogram();
